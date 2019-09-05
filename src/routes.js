23fef8552fe6f16d95c9956e2a4e63c006bcb4d7
brenno araujo queiroz <brennoaraujoqueiroz@gmail.com>
2019-09-05 02:14:55 -0300

    backend - 70%

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'teu cu' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Brenno Araujo',
+    email: 'brenno@test.com',
+    password_hash: '1234bbb',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
